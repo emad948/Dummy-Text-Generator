cd141638bbd6e67a2919903872fbef8ab5e68732
Emad <almsouti.e@gmail.com>
2021-04-13 17:07:01 +0200

    Typo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from 'react';
+import data from './data';
+import Button from '@material-ui/core/Button';
+import './index.css';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [input, setInput]=useState(0);
+    const [value, setValue]=useState([]);
+
+
+
+
+
+    const handleSubmit=(e) => {
+        e.preventDefault();
+        let count=parseInt(input)
+        if (count<0) {
+            count=0;
+        }
+        if (count>8) {
+            count=8;
+        }
+        setValue(data.slice(0, count));
+        //End is not included;)
+
+    }
+    return (
+        <>
+            <section className="section-center">
+                <h3>TIRED OF BORING LOREM IPSUM?</h3>
+                <form className="lorem-form" onSubmit={handleSubmit}>
+                    <label htmlFor="par">Paragraphs:</label>
+                    <input type="number" id='par' name="par" value={input} onChange={(e) => setInput(e.target.value)}></input>
+                    <Button type="submit" variant="contained" color="primary" >GENERATE  </Button>
+
+                </form>
+
+            </section>
+            <article className="lorem-text">
+                {value.map((item, index) => {
+                    return (
+                        <p key={index}>{item}</p>
+                    )
+                })}
+            </article>
+        </>
+    )
 }
 
 export default App;
